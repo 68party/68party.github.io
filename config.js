208ead18ce066a401edfdec4d9e802060a2e5258
wurh <wurh@users.noreply.github.com>
2015-05-19 16:32:42 +0800

    【INI】初始化

---
+++ config.js
@@ -0,0 +1,8 @@
+var config ={
+    page2:'http://www.68party.com/',
+    page3:'http://www.68party.com/',
+    page4:'http://www.68party.com/',
+    page5:'http://www.68party.com/',
+    page6:'http://www.68party.com/',
+    page7:'http://www.68party.com/'
+}
